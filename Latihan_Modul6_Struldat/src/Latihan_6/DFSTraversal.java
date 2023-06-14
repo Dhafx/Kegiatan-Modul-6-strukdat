951589fa2f5e0f6931f646436151447a2db3225c
M. Dhafa Maulana <120663181+Dhafx@users.noreply.github.com>
2023-06-14 23:15:39 +0700

    Update DFSTraversal.java

--- Latihan_Modul6_Struldat/src/Latihan_6/DFSTraversal.java
+++ Latihan_Modul6_Struldat/src/Latihan_6/DFSTraversal.java
@@ -25,28 +25,4 @@ class DFSTraversal {
                 DFS(n);
         }
     }
-    public static void main(String args[]) {
-        DFSTraversal graph = new DFSTraversal(12);
-        graph.insertEdge(1, 2);
-        graph.insertEdge(2, 1);
-        graph.insertEdge(2, 5);
-        graph.insertEdge(5, 2);
-        graph.insertEdge(5, 3);
-        graph.insertEdge(5, 9);
-        graph.insertEdge(9, 5);
-        graph.insertEdge(9, 8);
-        graph.insertEdge(8, 9);
-        graph.insertEdge(8, 11);
-        graph.insertEdge(11, 10);
-        graph.insertEdge(5, 7);
-        graph.insertEdge(7, 11);
-        graph.insertEdge(7, 6);
-        graph.insertEdge(6, 7);
-        graph.insertEdge(2, 1);
-        graph.insertEdge(1,4);
-        graph.insertEdge(4, 6);
-
-        System.out.println("Depth First Traversal for the graph is:");
-        graph.DFS(1);
-    }
 }
